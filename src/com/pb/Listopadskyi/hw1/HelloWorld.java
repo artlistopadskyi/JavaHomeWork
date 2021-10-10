eacd754c07fdb88b27797bb30fec46d61c0357c5
Artem Listopadskyi <artjom.listopadskij@privatbank.ua>
2021-10-10 14:03:29 +0300

    init commit

--- src/com/pb/Listopadskyi/hw1/HelloWorld.java
+++ src/com/pb/Listopadskyi/hw1/HelloWorld.java
@@ -2,6 +2,6 @@ package com.pb.Listopadskyi.hw1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.printf("Hello World");
+        System.out.println("Hello World");
     }
 }
