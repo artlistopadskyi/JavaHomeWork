4594c6954b187e1349bd6d29749c14b8e6164f64
Artem Listopadskyi <artjom.listopadskij@privatbank.ua>
2021-10-23 21:44:37 +0300

    init commit

--- src/hw3/Array.java
+++ src/hw3/Array.java
@@ -1,35 +1,31 @@
 package hw3;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
-        int[] array = new int[10];
-        int x = array.length;
-
-        for (int i = 0; i < array.length; i++) {
-            array[i] = i * 2;
+        System.out.println("Введите масив");
+        int[] arr = new int[10];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
         }
-
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
-        int myArray[] = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18};
         int sum = 0;
-        for (int i = 0; i < myArray.length; i++) {
-            sum = sum + myArray[i];
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
         }
         System.out.println("Сума массива:" + (sum));
-
-        int[] arr = {2,4,6,8,10,12,14,16,18};
+    
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 0) {
-                count++;
-            }
+        if (arr[i] > 0) {
+            count++;
         }
+    }
         System.out.println("В массиве " + Arrays.toString(arr));
         System.out.println(count + " положительных элементов");
+
     }
 }
 
