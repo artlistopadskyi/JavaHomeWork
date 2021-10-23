9d10a6e491bd8d64d8ffb9dfb0230515a7196e06
Artem Listopadskyi <artjom.listopadskij@privatbank.ua>
2021-10-23 22:49:15 +0300

    init commit

--- src/hw3/Array.java
+++ src/hw3/Array.java
@@ -3,31 +3,50 @@ package hw3;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Array {
+public class Array<isSorted> {
     public static void main(String[] args) {
         System.out.println("Введите масив");
-        int[] arr = new int[10];
+        int[] array = new int[10];
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = scanner.nextInt();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
         }
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum = sum + arr[i];
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
         }
         System.out.println("Сума массива:" + (sum));
-    
+
         int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-        if (arr[i] > 0) {
-            count++;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                count++;
+            }
         }
-    }
-        System.out.println("В массиве " + Arrays.toString(arr));
+        System.out.println("В массиве " + Arrays.toString(array));
         System.out.println(count + " положительных элементов");
 
+        boolean isSorted = false;
+        int buf;
+        while(!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    isSorted = false;
+
+                    buf = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = buf;
+                }
+            }
+        System.out.println("Сортировка пузырьком"+ Arrays.toString( array));
+        }
     }
 }
+
+
+
+
 
 
 
